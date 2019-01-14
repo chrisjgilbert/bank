16166b35d738ebe2909fc8891f5f68587775c93b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 23:47:04 +0000

    date formatter feature written

---
+++ spec/feature/date_formatter_feature_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'date_formatter'
+
+describe("date formatter") do
+  it("formats the date in DD/MM/YYYY") do
+    today = DateFormatter.new
+    expect(today.getDate).not_to raise_error
+  end
+end
