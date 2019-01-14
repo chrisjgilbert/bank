4201dd2319e3b8f449f30aba613aa20cdc895875
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 22:48:01 +0000

    user story 1 feature test written

---
+++ spec/feature/account_feature_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'account feature' do
+
+  context('transactions') do
+    it('allows a user to deposit funds into their account') do
+      account = Account.new
+      expect { account.deposit(5) }.not_to raise_error
+    end
+  end
+
+end
