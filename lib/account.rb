d8996a399ed8b53698aacf8a34d92ae6da531dd8
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 22:54:18 +0000

    green: #deposit adds amount to balance. user story 1 implemented

---
+++ lib/account.rb
@@ -0,0 +1,15 @@
+class Account
+
+  DEFAULT_BALANCE = 0
+
+  attr_reader :balance
+
+  def initialize
+    @balance = DEFAULT_BALANCE
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+end
