c14e7226e68f93b104966a4c760868805e0aa425
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-14 23:06:13 +0000

    withdrawing with insufficient funds feature test written

--- spec/feature/account_feature_spec.rb
+++ spec/feature/account_feature_spec.rb
@@ -8,9 +8,15 @@ describe 'account feature' do
       expect { account.deposit(5) }.not_to raise_error
     end
 
-    it('allows a user to withdraw funds into their account') do
+    it('allows a user to withdraw funds into their account if they have enough funds') do
       account = Account.new
+      account.deposit(10)
       expect { account.withdraw(5) }.not_to raise_error
+    end
+
+    it('does not allow a user to withdraw funds if they have insuficcient funds') do
+      account = Account.new
+      expect { account.withdraw(5) }.to raise_error
     end
   end
 end
