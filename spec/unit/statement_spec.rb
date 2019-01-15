045c16cf4e9c95383fdf4570ad100fb9fde54b91
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 09:54:30 +0000

    red: #statement prints out transaction history

--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -7,5 +7,11 @@ describe Statement do
       statement = Statement.new
       expect { statement.print }.to output("date || credit || debit || balance\n").to_stdout
     end
+
+    it('prints out the transaction history') do
+      transactions = [{date: "01/10/2010", credit: 100, balance: 100}]
+      statement = Statement.new
+      expect { statement.print(transactions) }.to output("date || credit || debit || balance\n").to_stdout
+    end
   end
 end
