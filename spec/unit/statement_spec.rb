b15175c090ee683e7e573d23d7dde6d2ccbfcc39
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 10:24:49 +0000

    test updated: #print prints out the transaction history with 2 decimal places

--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -6,7 +6,7 @@ describe Statement do
     it('prints out the transaction history') do
       transactions = [{date: "01/10/2010", credit: 100, balance: 100}]
       statement = Statement.new
-      expect { statement.print(transactions) }.to output("date || credit || debit || balance\n01/10/2010 || 100 ||  || 100\n").to_stdout
+      expect { statement.print(transactions) }.to output("date || credit || debit || balance\n01/10/2010 || 100.00 ||  || 100.00\n").to_stdout
     end
   end
 end
