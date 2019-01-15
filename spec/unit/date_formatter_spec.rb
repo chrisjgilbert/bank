cf90b005b430c99da7d48d13292d58d8289c735e
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 00:05:44 +0000

    green: date foramatter #getDate

--- spec/unit/date_formatter_spec.rb
+++ spec/unit/date_formatter_spec.rb
@@ -4,8 +4,11 @@ require 'date_formatter'
 describe DateFormatter do
 
   describe('#getDate') do
-    it("formats the date in DD/MM/YYYY") do
-      today = DateFormatter.new
+    it('formats the date in DD/MM/YYYY') do
+      date = double(:date)
+      today = DateFormatter.new(date)
+      allow(date).to receive(:today).and_return(date)
+      allow(date).to receive(:strftime).and_return("14/01/2019")
       expect(today.getDate).to eq("14/01/2019")
     end
   end
