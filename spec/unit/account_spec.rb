ca7e0723ee588bd0ea20cee6ced4544a55d5719a
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 09:32:35 +0000

    account_spec refactored into using let methods and before each

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -2,62 +2,55 @@ require 'spec_helper'
 require 'account'
 
 describe Account do
+
+  let(:statement) { double(:statement) }
+  let(:date_formatter) { double(:date_formatter, getDate: '01/01/2001') }
+  let(:subject) { described_class.new(statement, date_formatter) }
+
   context('transactions') do
     describe('#deposit') do
       it('adds amount to balance') do
-        account = Account.new
-        expect { account.deposit(10) }.to change { account.balance }.by(10)
+        expect { subject.deposit(10) }.to change { subject.balance }.by(10)
       end
 
       it('adds transaction to transactions array') do
-        account = Account.new
-        expect { account.deposit(100) }.to change { account.transactions.length }.by(1)
+        expect { subject.deposit(100) }.to change { subject.transactions.length }.by(1)
       end
 
       it('adds date to transaction') do
-        date_formatter = double(:date_formatter, getDate: '01/01/2001')
-        statement = double(:statement)
-        account = Account.new(statement, date_formatter)
         expect(date_formatter).to receive(:getDate)
-        account.deposit(10)
+        subject.deposit(10)
       end
     end
 
     describe('#withdraw') do
       context('with sufficient funds') do
+        before(:each) do
+          allow(subject).to receive(:insufficient_funds?).and_return(false)
+        end
         it('subtracts amount from balance ') do
-          account = Account.new
-          account.deposit(10)
-          expect { account.withdraw(10) }.to change { account.balance }.by(-10)
+          expect { subject.withdraw(10) }.to change { subject.balance }.by(-10)
         end
         it('adds transaction to transactions array') do
-          account = Account.new
-          account.deposit(100)
-          expect { account.withdraw(50) }.to change { account.transactions.length }.by(1)
+          subject.deposit(100)
+          expect { subject.withdraw(50) }.to change { subject.transactions.length }.by(1)
         end
         it('adds date to transaction') do
-          date_formatter = double(:date_formatter, getDate: '01/01/2001')
-          statement = double(:statement)
-          account = Account.new(statement, date_formatter)
-          allow(account).to receive(:insufficient_funds?).and_return(false)
           expect(date_formatter).to receive(:getDate)
-          account.withdraw(10)
+          subject.withdraw(10)
         end
       end
       context('with insuficcient funds') do
         it('raies an error') do
-          account = Account.new
-          expect { account.withdraw(10) }.to raise_error('Unable to withdraw: insufficient funds!')
+          expect { subject.withdraw(10) }.to raise_error('Unable to withdraw: insufficient funds!')
         end
       end
     end
 
     describe('#print_statement') do
       it('calls on statement to print') do
-        statement = double(:statement, print: 'print')
-        account = Account.new(statement)
         expect(statement).to receive(:print)
-        account.print_statement
+        subject.print_statement
       end
     end
   end
