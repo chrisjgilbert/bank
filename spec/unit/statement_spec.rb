8917137172ef50ef34b3eb43e1b5080d6915636c
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 10:38:32 +0000

    removed previous statement print test as now redundant

--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -3,12 +3,6 @@ require 'statement'
 
 describe Statement do
   describe('#print') do
-    it('prints out the transaction history') do
-      transactions = [{date: "01/10/2010", credit: 100, balance: 100}]
-      statement = Statement.new
-      expect { statement.print(transactions) }.to output("date || credit || debit || balance\n01/10/2010 || 100.00 ||  || 100.00\n").to_stdout
-    end
-
     it('prints out the transaction history in reverse order') do
       transactions = [{date: "01/10/2010", credit: 100, balance: 100}, {date: '02/11/2010', debit: 50, balance: 50}]
       statement = Statement.new
