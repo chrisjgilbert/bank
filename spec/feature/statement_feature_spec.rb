4ec43a59aafb77c4f61a0fab43414db875ba0d31
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 09:23:44 +0000

    statement feature test refactored correctly

--- spec/feature/statement_feature_spec.rb
+++ spec/feature/statement_feature_spec.rb
@@ -4,13 +4,7 @@ require 'statement'
 describe('statment') do
   it('prints out a statement') do
     statement = Statement.new
-    transactions = [
-      {
-        date: '01/10/2018',
-        credit: 10,
-        balance: 10
-      }
-    ]
+    transactions = [{ date: '01/10/2018', credit: 10, balance: 10 }]
     expect { statement.print(transactions) }.not_to raise_error
   end
 end
