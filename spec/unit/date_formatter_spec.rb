85432911e867cba4b47f1cb32fbb4444646dca44
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 09:34:59 +0000

    date_formatter_spec refactored to use let methods

--- spec/unit/date_formatter_spec.rb
+++ spec/unit/date_formatter_spec.rb
@@ -3,28 +3,24 @@ require 'date_formatter'
 
 describe DateFormatter do
   describe('#getDate') do
+    let(:date) { double(:date) }
+    let(:subject) { described_class.new(date) }
     it('formats the date in DD/MM/YYYY') do
-      date = double(:date)
-      today = DateFormatter.new(date)
       allow(date).to receive(:today).and_return(date)
       allow(date).to receive(:strftime).and_return('14/01/2019')
-      expect(today.getDate).to eq('14/01/2019')
+      expect(subject.getDate).to eq('14/01/2019')
     end
 
     it('calls today on date') do
-      date = double(:date)
-      today = DateFormatter.new(date)
       allow(date).to receive(:strftime)
       expect(date).to receive(:today).and_return(date)
-      today.getDate
+      subject.getDate
     end
 
     it('calls strftime on date') do
-      date = double(:date)
-      today = DateFormatter.new(date)
       allow(date).to receive(:today).and_return(date)
       expect(date).to receive(:strftime)
-      today.getDate
+      subject.getDate
     end
   end
 end
